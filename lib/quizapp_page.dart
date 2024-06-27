a470887e247e2bf7656740912ec027ea4514e948
sherifmaher <capsherif19@gmail>
2024-06-27 05:41:45 +0300

    add quizpage

---
+++ lib/quizapp_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuizappPage extends StatelessWidget {
+  const QuizappPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
